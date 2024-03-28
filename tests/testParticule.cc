a1dd0ca30b66be1e3a56e90b435e5306c6916fc1
Jason Grieder <jason@grieder.org>
2024-03-28 11:08:59 +0100

    réponses, test

--- tests/testParticule.cc
+++ tests/testParticule.cc
@@ -1,19 +1,12 @@
 #include <iostream>
-#include "../vecteur/Vecteur3D.h"
 #include "../particule/Particule.h"
 using namespace std;
 
 int main() {
-  Vecteur3D pos1(1,1,1);
-  Vecteur3D pos2(1,18.5,1);
-  Vecteur3D pos3(1,1,3.1);
-  Vecteur3D vit1(0,0,0);
-  Vecteur3D vit2(0,0.2,0);
-  Vecteur3D vit3(0,0,-0.5);
 
-  Particule p1(4.002602, pos1, vit1);
-  Neon p2(20.1797, pos2, vit2);
-  Argon p3(39.948, pos3, vit3);
+  Particule p1(4.002602, {1,1,1}, {0,0,0});
+  Neon p2(20.1797, {1,18.5,1}, {0,0.2,0});
+  Argon p3(39.948, {1,1,3.1}, {0,0,-0.5});
   Particule p4(p2);
 
   cout << p1 << endl;
