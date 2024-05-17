1692b4adf003699457e42b8b3dde50e99cbb5a03
AlbanSib <you@example.com>
2024-05-17 17:27:08 +0200

    Révision des méthodes du Systeme

--- Systeme.cc
+++ Systeme.cc
@@ -2,6 +2,7 @@
 #include "utils.h"
 #include "Particule.h"
 #include "Vecteur3D.h"
+#include <iomanip>
 
 using namespace std;
 
@@ -12,9 +13,8 @@ using namespace std;
 ostream& Systeme::affiche(ostream& sortie) const {
     if (particules_.size() == 0) {sortie << "Le système est vide.";}
     else {
-        sortie << "Le système est constitué des " << particules_.size() <<
-                  " particules suivantes: " << endl;
-        for (auto& p : particules_) {
+        sortie << "Le système est constitué des " << particules_.size() << " particules suivantes: " << endl;
+        for (auto const& p : particules_) {
             sortie << endl << *p;
         }
         sortie << endl;
@@ -31,55 +31,50 @@ void Systeme::supp_all() {
 }
 
 void Systeme::collision_paroi(Particule& p, size_t i) {
-    if (p.get_pos(0) < EPSILON) {
-        cout << "La particule " << i+1 << " rebondit sur la face 1" << endl;
-        p.set_pos(0, 2*p.get_pos(0)-EPSILON);
-        p.set_vit(0,-p.get_vit(0));
+    for (size_t j(0); i<=2; ++i) {
+        if (p.get_pos(j) < PRECISION) {
+            cout << "La particule " << i+1 << " rebondit sur la face " << j+1 << endl;
+            p.set_pos(j, 2*p.get_pos(j)-PRECISION);
+            p.set_vit(j,-p.get_vit(j));
+        }   
     }
-    if (p.get_pos(1) < EPSILON) {
-        cout << "La particule " << i+1 << " rebondit sur la face 2" << endl;
-        p.set_pos(1, 2*p.get_pos(1)-EPSILON);
-        p.set_vit(1,-p.get_vit(1));
-    }
-    if (p.get_pos(2) < EPSILON) {
-        cout << "La particule " << i+1 << " rebondit sur la face 3" << endl;
-        p.set_pos(2, 2*p.get_pos(2)-EPSILON);
-        p.set_vit(2,-p.get_vit(2));
-    }
-    if (p.get_pos(0) > enceinte_->get_l()-EPSILON) {
+    if (enceinte_->get_l() - p.get_pos(0) < PRECISION) {
         cout << "La particule " << i+1 << " rebondit sur la face 4" << endl;
-        p.set_pos(0,enceinte_->get_l()-EPSILON);
+        p.set_pos(0,enceinte_->get_l()-PRECISION);
         p.set_vit(0,-p.get_vit(0));
     }
-    if (p.get_pos(1) > enceinte_->get_p()-EPSILON) {
+    if (enceinte_->get_h() - p.get_pos(1) < PRECISION) {
         cout << "La particule " << i+1 << " rebondit sur la face 5" << endl;
-        p.set_pos(1,enceinte_->get_p()-EPSILON);
+        p.set_pos(1,enceinte_->get_h()-PRECISION);
         p.set_vit(1,-p.get_vit(1));
     }
-    if (p.get_pos(2) > enceinte_->get_h()-EPSILON) {
+    if (enceinte_->get_p() - p.get_pos(2) < PRECISION) {
         cout << "La particule " << i+1 << " rebondit sur la face 6" << endl;
-        p.set_pos(2,enceinte_->get_h()-EPSILON);
+        p.set_pos(2,enceinte_->get_p()-PRECISION);
         p.set_vit(2,-p.get_vit(2));
     }
 }
 
-void Systeme::collision_particules(Particule& p, size_t i) {
-    if (i < particules_.size()) {
-        for (i; i < particules_.size(); ++i) {
-            afficher_collision(p, i);
-            p.collision_particule(*particules_[i], tirage_);
-            afficher_collision(p, i);
+void Systeme::collision_particules(Particule& p, size_t j) {
+    if (j < particules_.size()) {
+        for (size_t i(j); i < particules_.size(); ++i) {
+            if (p.test_contact(*particules_[i])) {
+                cout << "La particule " << i+1 << " entre collision avec une autre particule." << endl;
+                cout << setw(1) << "avant le choc : " << endl;  
+                afficher_collision(p, i);
+                p.collision_particule(*particules_[i], tirage_);
+                cout << setw(1) << "après le choc : " << endl; 
+                afficher_collision(p, i);
+            }
         }
     }
 }
 
 void Systeme::afficher_collision(Particule const& p, size_t i) const{
-     if (p.test_contact(*particules_[i])) {
-        cout << "La particule " << i+1
-               << " entre collision avec une autre particule." << endl
-               << "avant le choc : " << endl << "part. " << i+1 << " : : "
-               << *particules_[i] << endl << "autre : : " << p << endl;
-    }
+        cout << setw(3) << "part. " << i+1 << " : : "; 
+        particules_[i]->Particule::affiche(cout) << endl; 
+        cout << setw(3) << "autre : : "; 
+        p.Particule::affiche(cout) << endl;
 }
 
 void Systeme::evolue(double dt) {
@@ -88,12 +83,11 @@ void Systeme::evolue(double dt) {
         particules_[i]->evolue(dt);
         collision_paroi(*particules_[i], i);
         collision_particules(*particules_[i], i+1);
-        cout << particules_[i] << endl;
     }
+    cout << this; 
 }
 
-void Systeme::initialisation(double masse, uint temperature, enum type_particule,
-                            GenerateurAleatoire tirage) {
+void Systeme::initialisation(double masse, uint temperature, enum type_particule, GenerateurAleatoire tirage) {
     double pos_x(tirage.uniforme(0.0,enceinte_->get_l()));
     double pos_y(tirage.uniforme(0.0,enceinte_->get_p()));
     double pos_z(tirage.uniforme(0.0,enceinte_->get_h()));
@@ -102,8 +96,7 @@ void Systeme::initialisation(double masse, uint temperature, enum type_particule
     double vit_y(tirage.gaussienne(0.0,sqrt(k_B * temperature / masse)));
     double vit_z(tirage.gaussienne(0.0,sqrt(k_B * temperature / masse)));
 
-    Particule* p(new Particule(masse, {pos_x, pos_y, pos_z},
-                                      {vit_x ,vit_y ,vit_z }));
+    Particule* p(new Particule(masse, {pos_x, pos_y, pos_z}, {vit_x ,vit_y ,vit_z }));
     ajouter_particule(p);
 }
 
@@ -111,3 +104,4 @@ ostream& operator<<(ostream& sortie, Systeme const& S) {
     S.affiche(sortie);
     return sortie;
 }
+     
