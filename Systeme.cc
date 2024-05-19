411195ed7008e5414dce75a3f29843107390fd25
AlbanSib <you@example.com>
2024-05-19 21:50:01 +0200

    Modifs exoP9

--- Systeme.cc
+++ Systeme.cc
@@ -62,7 +62,7 @@ void Systeme::collision_particules(Particule& p, size_t j) {
                 cout << "La particule " << i+1 << " entre collision avec une autre particule." << endl;
                 cout << setw(1) << "avant le choc : " << endl;  
                 afficher_collision(p, i);
-                p.collision_particule(*particules_[i], tirage_);
+                particules_[i]->collision_particule(p, tirage_);
                 cout << setw(1) << "après le choc : " << endl; 
                 afficher_collision(p, i);
             }
@@ -83,8 +83,8 @@ void Systeme::evolue(double dt) {
         particules_[i]->evolue(dt);
         collision_paroi(*particules_[i], i);
         collision_particules(*particules_[i], i+1);
+        cout << *particules_[i] << endl; 
     }
-    cout << this; 
 }
 
 void Systeme::initialisation(double masse, uint temperature, enum type_particule, GenerateurAleatoire tirage) {
